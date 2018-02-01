226891b01988fbc5e87342b4d5cefbc5a5eca05b
ecp-alex <alex.mackenzie@englishcollege.cz>
2018-02-01 01:51:49 +0100

    THEME-CHILD | content.php | news item layout + css

--- wp-content/themes/ecp-child/template-parts/content.php
+++ wp-content/themes/ecp-child/template-parts/content.php
@@ -17,44 +17,53 @@
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
-
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php ecp_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
+		?>		
 	</header><!-- .entry-header -->
 
-	
-	<?php
-		if ( has_post_thumbnail() ) {
-			echo('<div class="post-featured-image">');
-			the_post_thumbnail();
-			echo('</div>');
-		}
-	?>
+	<div class="amcust-news-wrap">
+		<div class="col-brief">
+			<?php
+			if ( 'post' === get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php ecp_posted_on(); ?>
+			</div><!-- .entry-meta -->
+			<?php
+			endif; ?>
 
-	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'ecp' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'ecp' ),
-				'after'  => '</div>',
-			) );
-		?>
-
-		<div class="post-sharing-icons">
-			<span class="dashicons"><a href="https://twitter.com/intent/tweet?url=<?php echo(urlencode( get_post_permalink() )) ?>&text=<?php echo( urlencode(get_the_title() ) ) ?>"></a></span>
-			<span class="dashicons"><a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo(urlencode( get_post_permalink() )) ?>"></a></span>
+			<div class="contactus-social-icons-wrap text-align-left">
+				<h4>Share this article</h4>
+				<div class="contactus-social-icons">
+					<a class="social disc" title="Share on Twitter" href="https://twitter.com/intent/tweet?url=<?php echo(urlencode( get_post_permalink() )) ?>&text=<?php echo( urlencode(get_the_title() ) ) ?>"><i class="fa fa-lg fa-twitter"></i></a>
+					<a class="social disc" title="Share on Facebook" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo(urlencode( get_post_permalink() )) ?>"><i class="fa fa-lg fa-facebook"></i></a>
+				</div>
+			</div>
+			
 		</div>
+		<div class="col-content">
+			<?php
+				if ( has_post_thumbnail() ) {
+					echo('<div class="post-featured-image">');
+					the_post_thumbnail();
+					echo('</div>');
+				}
+			?>
 
-	</div><!-- .entry-content -->
+			<div class="entry-content">
+				<?php
+					the_content( sprintf(
+						/* translators: %s: Name of current post. */
+						wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'ecp' ), array( 'span' => array( 'class' => array() ) ) ),
+						the_title( '<span class="screen-reader-text">"', '"</span>', false )
+					) );
+
+					wp_link_pages( array(
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'ecp' ),
+						'after'  => '</div>',
+					) );
+				?>
+			</div><!-- .entry-content -->
+		</div>
+	</div>
 
 	<footer class="entry-footer">
 		<?php ecp_entry_footer(); ?>
