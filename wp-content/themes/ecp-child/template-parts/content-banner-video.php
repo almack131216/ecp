bcc4d2b13601ad078c83bbd7cbafd322dccf83f5
ecp-alex <alex.mackenzie@englishcollege.cz>
2021-12-08 15:49:11 +0100

    FUNC | add video thumbnails to homepage

--- wp-content/themes/ecp-child/template-parts/content-banner-video.php
+++ wp-content/themes/ecp-child/template-parts/content-banner-video.php
@@ -6,6 +6,29 @@
  */
 ?>
 
+<?php
+$amcust_tmpLang = get_bloginfo('language');
+// echo 'amcust_tmpLang: ' . $amcust_tmpLang;
+$titleArr = array("title"=>"Videos", "see_all_link"=>"/ecp-people", "see_all_text"=>"See All Videos");
+$videosArr = [];
+$videosArr[] = array("title"=>"ECP Admission Process","v"=>"si792If9UP4");
+$videosArr[] = array("title"=>"ECP Admission Process 2","v"=>"si792If9UP4");
+
+if($amcust_tmpLang=='cs-CZ') {
+	$titleArr = array("title"=>"Videos CZ", "see_all_link"=>"/ecp-people", "see_all_text"=>"See All Videos CZ");
+}
+
+function PrintPreview($getVideoArr){
+	$previewBtn = '<a href="#" class="preview" title="ECP Admission Process">';
+	$previewBtn .= '<img src="http://img.youtube.com/vi/si792If9UP4/0.jpg">';
+	$previewBtn .= '<i class="fa fa-youtube-play"></i>';
+	$previewBtn .= '<span>ECP Admission Process</span>';
+	$previewBtn .= '</a>';
+	return $previewBtn;
+}
+
+?>
+
 <div id="video-container">
 	<!-- The video -->
 	<video autoplay muted loop
@@ -22,21 +45,34 @@
 
 
 	<!-- Optional: some overlay text to describe the video -->
-	<div class="video-content">
+	<div class="video-content basic display-none">
 		<h1>Come and join us!</h1>
 		<!-- <p>Lorem ipsum...</p> -->
 		<!-- Use a button to pause/play the video with JavaScript -->
-		<button id="btnPlayPause" onclick="videoPlayPause()" class="video-button"><i class="fa fa-pause"></i></button>
+		<!-- <button id="btnPlayPause" onclick="videoPlayPause()" class="video-button"><i class="fa fa-pause"></i></button>
 		<button id="btnMuteUnmute" onclick="videoMuteUnmute()" class="video-button"><i class="fa fa-volume-up"></i></button>
-		<a id="btnGoToFullVideo" href="<?php echo get_site_url(); ?>/ecp-video/" class="video-button">Watch full video</a>
+		<a id="btnGoToFullVideo" href="<?php echo get_site_url(); ?>/ecp-video/" class="video-button">Watch full video 1</a> -->
 		<span class="video-button-parent">
-		<?php echo do_shortcode("[video_lightbox_youtube video_id='C9E8K-4IDrk&rel=false&autoplay=1' auto_play='true' width='640' height='480' class='btnWatchFullVideo' anchor='Watch full video']"); ?>
-		<?php echo do_shortcode("[video_lightbox_youtube video_id='C9E8K-4IDrk&rel=false&autoplay=1&cc_load_policy=1&cc_lang_pref=cs' auto_play='true' width='640' height='480' class='btnWatchFullVideo' anchor='Watch full video']"); ?>
-		<?php echo do_shortcode("[wp-video-popup video='https://www.youtube.com/watch?v=C9E8K-4IDrk&rel=false&autoplay=1&cc_load_policy=1&cc_lang_pref=cs']"); ?>		
+		<?php
+			//echo do_shortcode("[video_lightbox_youtube video_id='C9E8K-4IDrk&rel=0&autoplay=1' auto_play='true' width='640' height='480' class='btnWatchFullVideo' anchor='Watch full video 2']");
+			// echo do_shortcode("[video_lightbox_youtube video_id='C9E8K-4IDrk&rel=0&autoplay=1&cc_load_policy=1&cc_lang_pref=cs' auto_play='true' width='640' height='480' class='btnWatchFullVideo' anchor='Watch full video 3']");
+			echo do_shortcode("[wp-video-popup video='https://www.youtube.com/watch?v=C9E8K-4IDrk&rel=0&autoplay=1&cc_load_policy=1&cc_lang_pref=cs']");
+		?>
 		<a href="#" class="wp-video-popup">Play Video</a>
-		<a rel="lightbox" data-gall="gall-frame" data-lightbox-type="inline" href="#inline-content">RL iframe</a>
-		<a href="https://www.youtube.com/watch?v=C9E8K-4IDrk&rel=false&autoplay=1&cc_load_policy=1&cc_lang_pref=cs" rel="lightbox" data-lightbox-type="iframe">RL YouTube</a>
+		<!-- <a rel="lightbox" data-gall="gall-frame" data-lightbox-type="inline" href="#inline-content">RL iframe</a> -->
+		<!-- <a href="https://www.youtube.com/watch?v=C9E8K-4IDrk&rel=false&autoplay=1&cc_load_policy=1&cc_lang_pref=cs" rel="lightbox" data-lightbox-type="iframe">RL YouTube</a> -->
 		</span>
+	</div>
+	<div class="video-content thumbnails">
+		<?php
+			echo do_shortcode('[title_splitter title="'.$titleArr['title'].'" see_all_link="'.$titleArr['see_all_link'].'" see_all_text="'.$titleArr['see_all_text'].'" see_all_class="none" class="white"]');		
+		?>
+		<div class="previews-wrap">
+			<?php
+				echo PrintPreview($videosArr[0]);
+				echo PrintPreview($videosArr[1]);
+			?>			
+		</div>
 	</div>
 </div>
 
